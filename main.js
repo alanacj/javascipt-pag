cdfa2412b5a76af44f362bc4876d15911d2c3ce9
Alana Carolini <105944410+alanacj@users.noreply.github.com>
2023-09-22 11:54:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
+function tocaSom (idElementoAudio) {
+     document.querySelector(idElementoAudio).play();
 }
 
-const listaDeTeclas =document.querySelectorAll('.tecla');
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
+let contador = 0;
+
+//enquanto
+while (contador < listaDeTeclas.length) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    console.log(instrumento);
+
+    const idAudio
+
+    tecla.onclick = function () {
+        tocaSom ();
+    }
+
+    contador = contador + 1;
+
+    console.log(contador);
+
+}
